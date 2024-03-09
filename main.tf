286fec3c766a21d5ef70637ea07c6e82f3d5b716
Marcelo Arguello <marcelo.arguello@gmail.com>
2024-03-09 13:16:31 -0300

    Modules implemented successfuly

--- main.tf
+++ main.tf
@@ -1,61 +1,49 @@
 provider "aws" {
     region = "us-east-1"
-    profile = "terraform-elrond"
+    profile = "terraform-isildur"
 }
 
-resource "aws_instance" "terraform_deployer" {
-    ami = "ami-0e1d30f2c40c4c701"
-    instance_type = "t2.micro"
-    key_name = "elrond"
-    security_groups = [aws_security_group.aws_sg.name]
-    vpc_security_group_ids      = [aws_security_group.aws_sg.id]
-    associate_public_ip_address = true
-    user_data = file("server-script.sh")
-    tags = {
-        Name = "deployer"
-    }
+data "aws_vpc" "default_vpc" {
+  default = true
+} 
+
+#Create security group for EC2
+### Security Group Module
+module "ec2_sg" {
+    source = "./sg"
+    appName = var.appName
+    vpc_id = data.aws_vpc.default_vpc.id
 }
 
-
-resource "aws_security_group" "aws_sg" {
-  name = "security group from terraform"
-
-  ingress {
-    description = "SSH from the internet"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "80 from the internet"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+# Terraform module Block to create the iam instance profile
+module "iam_instance_profile" {
+  source                = "./iam"
+  instance_profile_name = "instance-profile-devops"
+  iam_policy_name       = "devops-policy"
+  role_name             = "role_name"
 }
 
+# Terraform module Block to create EC2 (kubectl)
+module "ec2" {
+  source        = "./ec2"
+  ec2_name      = "terraform_deployer"
+  key_name      = var.key_name
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_sg        = module.ec2_sg.deployer_sg_id
+  instance_profile = module.iam_instance_profile.instance_profile
+}
 
 output "PrivateIp_Instance" {
-    value = aws_instance.terraform_deployer.private_ip
+    value = module.ec2.ec2_private_ip
 }
 
 output "PublicIp_Instance" {
-    value = aws_instance.terraform_deployer.public_ip
+    value = module.ec2.ec2_public_ip
 }
 
-
 data "aws_caller_identity" "current" {}
+
 output "account_id" {
     value = data.aws_caller_identity.current.account_id
 }
